f92342f749baa56fa0ecaf7ee78b094a81e99c02
vitaliifedunyk <vitaliifedunyk.dev@gmail.com>
2026-01-02 13:21:48 +0100

    Update Netlify configuration and HTML assets; remove obsolete netlify.toml file from work-calendar-2026

--- work-calendar-2026/vite.config.js
+++ work-calendar-2026/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE_URL || "/calendar2026/",
+  base: process.env.BASE_URL || "/",
 });
